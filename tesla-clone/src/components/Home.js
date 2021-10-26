c8ff679051111568fb429cc3071c067a69dca203
UmarkhonAkhmedov <umarkhonakhmedov2000@gmail.com>
2021-10-26 18:35:53 +0500

    Creating folder with some js files in it

---
+++ tesla-clone/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import Section from './Section';
+
+function Home() {
+  return (
+    <Container>
+      <Section/>
+    </Container>
+  )
+}
+
+export default Home
+
+
+const Container = styled.div`
+  height: 100vh;
+`
---
+++ tesla-clone/src/components/Section.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Section() {
+  return (
+    <Wrap>
+      Section
+    </Wrap>
+  )
+}
+
+export default Section
+
+const Wrap = styled.div`
+  
+`
