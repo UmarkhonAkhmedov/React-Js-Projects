f66b1bf039fa087a5988549ce69f4da3b3b03bdc
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-11-04 04:51:12 +0500

    Create Header Component  and Home Component

---
+++ disney-plus-clone/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src=""/>
+      <navMenu>
+        
+      </navMenu>
+
+    </Nav>
+  )
+}
+
+export default Header
+
+const Nav = styled.nav`
+
+`
+
+const Logo = styled.img`
+  width: 80px;
+
+`
+
+const navMenu = styled.div`
+  
+`
