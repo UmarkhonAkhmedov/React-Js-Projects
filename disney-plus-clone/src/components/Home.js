888bdc72797193a2edb9c0d3e433c13f33e5cd3c
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-11-11 03:35:58 +0500

    Build & Style the Home Component

--- disney-plus-clone/src/components/Home.js
+++ disney-plus-clone/src/components/Home.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Home() {
   return (
-    <div>
+    <Container>
       
-    </div>
+    </Container>
   )
 }
 
 export default Home
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: url("/images/home-background.png") center center / cover no-repeat fixed;
+    z-index: -1;
+  }
+`
