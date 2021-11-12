15e6fd6793ab8666fbeadcb0f67f727b9d41d93b
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-11-13 03:39:20 +0500

    Build & Style Detail Component

--- disney-plus-clone/src/App.js
+++ disney-plus-clone/src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Counter } from './features/counter/Counter';
 import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import Header from './components/Header'
+import Home from './components/Home'
+import Detail from './components/Detail'
+import Login from './components/Login'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- disney-plus-clone/src/components/Detail.js
+++ disney-plus-clone/src/components/Detail.js
@@ -1,11 +1,142 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Detail() {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Background>
+      <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg" />
+      </Background>
+      <ImageTitle>
+        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78"/>
+      </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png"/>
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png"/>
+          <span>Trailer</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png"/>
+        </GroupWatchButton>
+      </Controls>
+      <SubTitle>
+        2018 - 7m - Family, Fantasy, Kids, Animation
+      </SubTitle>
+      <Description>
+        A Chinese mom who's sad when her grown son leaves home gets another chance at motherhood when one of her dumplings springs to life. But she finds that nothing stays cute ans small forever.
+      </Description>
+    </Container>
   )
 }
 
 export default Detail
+
+const Container = styled.div`
+  position: relative;
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  
+`
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  min-height: 170px;
+  width: 35vw;
+  min-width: 200px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+
+`
+
+const PlayButton = styled.button`
+  display: flex;
+  align-items: center;
+  padding: 0 24px;
+  margin-right: 22px;
+  height: 56px;
+  border-radius: 4px;
+  font-size: 15px;
+  background: rgb(249, 249, 249);
+  border: none;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+
+  &:hover {
+    background: rgba(198, 198, 198);
+  }
+`
+
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`
+
+const AddButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  width: 44px;
+  height: 44px;
+  cursor: pointer;
+  margin-right: 16px;
+
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`
+
+const GroupWatchButton = styled(AddButton)`
+  background: rgb(0, 0, 0);
+`
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+`
+
+const Description = styled.div`
+  font-size: 20px;
+  margin-top: 16px;
+  line-height: 1.4;
+  color: rgb(249, 249, 249);
+`
