48deb15dec3316d95ca3c32c30e95e065c71114b
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-11-15 16:27:16 +0500

    Displaying data dynamically in Detail Component

--- disney-plus-clone/src/components/Detail.js
+++ disney-plus-clone/src/components/Detail.js
@@ -1,37 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { useParams} from "react-router-dom"
+import db from "../firebase"
 
 function Detail() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState()
+
+  useEffect(()=>{
+    // Grab the movie info from DB
+    db.collection("movies").doc(id).get().then((doc)=>{
+      if(doc.exists){
+        // Save the movie data
+        setMovie(doc.data());
+      } else {
+        // Redirect to home page
+      }
+    })
+  }, [id])
+
   return (
     <Container>
-      <Background>
-      <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg" />
-      </Background>
-      <ImageTitle>
-        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78"/>
-      </ImageTitle>
-      <Controls>
-        <PlayButton>
-          <img src="/images/play-icon-black.png"/>
-          <span>PLAY</span>
-        </PlayButton>
-        <TrailerButton>
-          <img src="/images/play-icon-white.png"/>
-          <span>Trailer</span>
-        </TrailerButton>
-        <AddButton>
-          <span>+</span>
-        </AddButton>
-        <GroupWatchButton>
-          <img src="/images/group-icon.png"/>
-        </GroupWatchButton>
-      </Controls>
-      <SubTitle>
-        2018 - 7m - Family, Fantasy, Kids, Animation
-      </SubTitle>
-      <Description>
-        A Chinese mom who's sad when her grown son leaves home gets another chance at motherhood when one of her dumplings springs to life. But she finds that nothing stays cute ans small forever.
-      </Description>
+      {movie && 
+        (
+          <>
+            <Background>
+              <img src={movie.backgroundImg} />
+            </Background>
+            <ImageTitle>
+              <img src={movie.titleImg}/>
+            </ImageTitle>
+            <Controls>
+            <PlayButton>
+              <img src="/images/play-icon-black.png"/>
+              <span>PLAY</span>
+            </PlayButton>
+            <TrailerButton>
+              <img src="/images/play-icon-white.png"/>
+              <span>Trailer</span>
+            </TrailerButton>
+            <AddButton>
+              <span>+</span>
+            </AddButton>
+            <GroupWatchButton>
+              <img src="/images/group-icon.png"/>
+            </GroupWatchButton>
+          </Controls>
+          <SubTitle>
+            {movie.subTitle}
+          </SubTitle>
+          <Description>
+            {movie.description}
+          </Description>
+        </>
+        )
+      }
+      
     </Container>
   )
 }
