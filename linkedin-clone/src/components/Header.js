39812a3dc6da3b645d6bfd3582f380baacff3f37
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-11-23 10:42:59 +0500

    Styling Nav and Adding user and work items of navabr

--- linkedin-clone/src/components/Header.js
+++ linkedin-clone/src/components/Header.js
@@ -16,7 +16,7 @@ const Header = (props) => {
           </Search>
           <Nav>
             <NavListWrap>
-              <NavList>
+              <NavList className="active">
                 <a>
                   <img src="/images/nav-home.svg" alt="Home Icon"/>
                   <span>Home</span>
@@ -46,6 +46,23 @@ const Header = (props) => {
                   <span>Notifications</span>
                 </a>
               </NavList>
+              <User>
+                <a>
+                  <img src="/images/user.svg" alt=""/>
+                  <span>Me</span>
+                  <img src="/images/down-icon.svg" alt=""/>
+                </a>
+                <SignOut>
+                  <a>Sign Out</a>
+                </SignOut>
+              </User>
+              <Work>  
+                <a>
+                  <img src="/images/nav-work.svg" alt=""/>
+                  <span>Work</span>
+                  <img src="/images/down-icon.svg"/>
+                </a>
+              </Work>
             </NavListWrap>
           </Nav>
         </Content>
@@ -129,6 +146,20 @@ const NavListWrap = styled.ul`
   display: flex;
   flex-wrap: nowrap;
   list-style-type: none;
+
+  .active {
+    span:after {
+      content: '';
+      transform: scaleX(1);
+      border-bottom: 2px solid var(--white, #fff);
+      bottom: 0;
+      left: 0;
+      position: absolute;
+      transition: transform 0.2 ease-in-out;
+      width: 100%;
+      border-color: rgba(0, 0, 0, 0.9);
+    }
+  }
 `
 const NavList = styled.li`
   display: flex;
@@ -165,3 +196,40 @@ const NavList = styled.li`
     }
   }
 `
+const SignOut = styled.div`
+  position: absolute;
+  top: 45px;
+  background: white;
+  border-radius: 0 0 5px 5px;
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  transition-duration: 167ms;
+  text-align: center;
+  display: none;
+`
+const User = styled(NavList)`
+  a > svg {
+    width: 24px;
+    border-radius: 50%;
+  }
+  a > img {
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+  }
+  span {
+    display: flex;
+    align-items: center;
+  }
+  &:hover {
+    ${SignOut}{
+      align-items: center;
+      display: flex;
+    }
+  }
+`
+const Work =styled(User)`
+  border-left: 1px solid rgba(0, 0, 0, 0.08);
+`
+
