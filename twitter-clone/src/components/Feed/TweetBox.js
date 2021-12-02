8fe346fe228ca53fa206dd9924cf3a54fdaa3629
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-12-02 08:28:55 +0500

    Finishing TweetBox file

--- twitter-clone/src/components/Feed/TweetBox.js
+++ twitter-clone/src/components/Feed/TweetBox.js
@@ -10,9 +10,9 @@ function TweetBox() {
       <form>
         <div className="tweetBox__input">
           <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
-          <input placeholder="What's happening?" type="text"/>
+          <input className="tweetBox__imageInput" placeholder="Enter image URL" type="text"/>
         </div>
-        <Button>Tweet</Button>
+        <Button className="tweetBox__tweetButton">Tweet</Button>
       </form>
     </div>
   )
