4442ca90b8ca75c094f8a3b4781271521692c825
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-12-04 20:36:01 +0500

    Adding and Styling widgets

--- twitter-clone/src/components/Widgets/Widgets.js
+++ twitter-clone/src/components/Widgets/Widgets.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import './Widgets.css'
+import React from "react";
+import "./Widgets.css";
+// import {
+//   TwitterTimelineEmbed,
+//   TwitterShareButton,
+//   TwitterTweetEmbed,
+// } from "@types/react-twitter-embed";
+import SavedSearchOutlinedIcon from '@mui/icons-material/SavedSearchOutlined';
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SavedSearchOutlinedIcon className="widgets__serachIcon"/>
+        <input placeholder="Search Twiiter" type="text"/>
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+        {/* <TwitterTweetEmbed tweetId={"858551177860055040"}/> */}
+        {/* <TwitterTimelineEmbed sourceType="profile" screenName="cleverqazi" options={{height: 400}}/> */}
+        {/* <TwitterShareButton url={"https://facebook.com/cleverprogranner"} options={{text: "#reactjs is awesome", via: "cleverqazi"}}/> */}
+      </div>
     </div>
   )
 }
