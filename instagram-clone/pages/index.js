582210220314911b4711641bc8ede43373ff1758
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-12-11 15:07:23 +0500

    Starting new project - instagram clone with next js, creating some files

---
+++ instagram-clone/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>This is the Instagram</h1>
+      
+    </div>
+  )
+}
