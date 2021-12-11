9da5420197e47d8d40eedce7521d069688925865
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-12-11 16:20:43 +0500

    Creating Logo and Search parts of Header Section  with responsive

--- instagram-clone/pages/index.js
+++ instagram-clone/pages/index.js
@@ -1,13 +1,18 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Instagram</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>This is the Instagram</h1>
+      {/* Header */}
+      <Header/>
+      {/* Feed */}
+
+      {/* Modal */}
       
     </div>
   )
