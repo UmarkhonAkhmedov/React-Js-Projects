d4fa7e639f1d10ea59c2cc9774435dfa5712df89
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-12-11 16:51:11 +0500

    Adding Right part of Header Section and finishing header section

--- instagram-clone/components/Header.js
+++ instagram-clone/components/Header.js
@@ -3,17 +3,18 @@ import Image from "next/image"
 import {
   SearchIcon,
   PlusCircleIcon,
-  userGroupIcon,
+  UserGroupIcon,
   HeartIcon,
   PaperAirplaneIcon,
   MenuIcon,
   SearchCircleIcon,
 } from "@heroicons/react/outline"
+import { HomeIcon } from "@heroicons/react/solid"
 
 function Header() {
   return (
-    <div>
-      <div className="flex justify-between max-w-6xl">
+    <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* Left */}
         <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image src="https://links.papareact.com/ocw" layout="fill" objectFit="contain"/>
@@ -23,16 +24,31 @@ function Header() {
         </div>
 
         {/* Middle */}
-        <div className='relative mt-1 p-3 rounded-md'>
-          <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
-            <SearchIcon className='h-5 w-5 text-gray-500'/>
+        <div className='max-w-xs'>
+          <div className='relative mt-1 p-3 rounded-md'>
+            <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+              <SearchIcon className='h-5 w-5 text-gray-500'/>
+            </div>
+            <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 
+            focus:ring-black focus:border-black rounded-md' 
+            type="text" placeholder="Search"/>
           </div>
-          <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 
-          focus:ring-black focus:border-black rounded-md' 
-          type="text" placeholder="Search"/>
         </div>
 
         {/* Right */}
+        <div className='flex items-center justify-end space-x-4'>
+          <HomeIcon className='navBtn'/>
+          <MenuIcon className='h-6 md:hidden cursor-pointer'/>
+          <div className='relative navBtn'>
+            <PaperAirplaneIcon className='navBtn rotate-45'/>
+            <div className='absolute -top-2 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex justify-center  text-white'>3</div>
+          </div>
+          <PlusCircleIcon className='navBtn'/>
+          <UserGroupIcon className='navBtn'/>
+          <HeartIcon className='navBtn'/>
+          <img src="https://avatars.githubusercontent.com/u/76143018?v=4" alt="Profile Picture"
+          className='h-10 rounded-full cursor-pointer'/>
+        </div>
       </div>
     </div>
   )
