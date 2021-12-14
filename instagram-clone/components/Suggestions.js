60bd32363ddf9b500781b26df798fc4a1abf8886
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-12-14 16:26:36 +0500

    Adding Suggetions section

--- instagram-clone/components/Suggestions.js
+++ instagram-clone/components/Suggestions.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import faker from 'faker'
+
 
 function Suggestions() {
+  const [suggestions, setSuggestions] = useState([])
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => (
+      {
+        ...faker.helpers.contextualCard(),
+        id: i
+      }
+    ))
+
+    setSuggestions(suggestions);
+  }, [])
+
   return (
-    <div>
-      
+    <div className='mt-4 ml-10'>
+      <div className='flex justify-between text-sm mb-5'>
+        <h3 className='text-sm font-bold text-gray-400'>Suggestions for you</h3>
+        <button className='text-gray-600 font-semibold'>See All</button>
+      </div>
+      {
+        suggestions.map(profile => (
+          <div key={profile.id} className='flex items-center justify-between mt-3'>
+            <img className='w-10 h-10 rounded-full border p-[2px]' src="https://avatars.githubusercontent.com/u/76143018?v=4" alt=""/>
+            <div className='flex-1 ml-4'>
+              <h2 className='font-semibold text-sm'>{profile.username}</h2>
+              <h3 className='text-xs text-gray-400'>Works ar {profile.company.name}</h3>
+            </div>
+            <button className='text-blue-400 text-xs font-bold'>Follow</button>
+          </div>
+    ))}
     </div>
   )
 }
