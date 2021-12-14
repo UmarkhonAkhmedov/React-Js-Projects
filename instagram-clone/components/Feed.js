f45c108dc4c44f301ac3f99775f7721e4673244a
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-12-14 16:09:17 +0500

    Finishing MiniProfile Section

--- instagram-clone/components/Feed.js
+++ instagram-clone/components/Feed.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import MiniProfile from './MiniProfile'
 import Posts from './Posts'
 import Stories from './Stories'
+import Suggestions from './Suggestions'
 
 
 function Feed() {
@@ -14,8 +16,11 @@ function Feed() {
 
 
       {/* Mini profile and Suggestions */}
-      <section>
-
+      <section className='hidden xl:inline-grid md: col-span-1'>
+        <div className='fixed top-20'>
+          <MiniProfile/>
+          <Suggestions/>
+        </div>
       </section>
     </main>
   )
---
+++ instagram-clone/components/Suggestions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Suggestions() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Suggestions
