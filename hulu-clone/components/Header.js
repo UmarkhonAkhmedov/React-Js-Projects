33d0e192b6e05a9937c72e3777ec2b5c0c06ec8a
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-12-21 00:06:16 +0500

    Finishing header items

--- hulu-clone/components/Header.js
+++ hulu-clone/components/Header.js
@@ -2,21 +2,26 @@ import Image from "next/image"
 import HeaderItem from "./HeaderItem"
 import {
   BadgeCheckIcon,
-  Collectionicon,
+  CollectionIcon,
   HomeIcon,
-  LightingBoltIcon,
+  LightningBoltIcon,
   SearchIcon,
   UserIcon,
 } from "@heroicons/react/outline"
 
 function Header() {
   return (
-    <header className="">
-      <div>
-        <HeaderItem title='Home' Icon={HomeIcon}/>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title='HOME' Icon={HomeIcon}/>
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon}/>
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon}/>
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon}/>
+        <HeaderItem title='SEARCH' Icon={SearchIcon}/>
+        <HeaderItem title='ACCOUNT' Icon={UserIcon}/>
+
       </div>
       <Image className="object-contain" src="https://links.papareact.com/ua6" width={200} height={100}/>
-      <p className="text-gray-500">uhfefiejfioefjoeifjoef</p>
     </header>
   )
 }
--- hulu-clone/components/HeaderItem.js
+++ hulu-clone/components/HeaderItem.js
@@ -1,8 +1,8 @@
 function HeaderItem({Icon, title}) {
   return (
-    <div className="flex flex-col group">
-      <Icon className="h-8 mb-1"/>
-      <p className="opacity-100 group-hover:opacity-100 tracking-widesty">{title}</p>
+    <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
+      <Icon className="h-8 mb-1 group-hover:animate-bounce"/>
+      <p className="opacity-0 group-hover:opacity-100 tracking-widesty">{title}</p>
     </div>
   )
 }
