263d0532a4350f63cd40f596a13868a12f1cde6c
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-05 16:34:28 +0500

    Customizing the Sign In Page

--- spotify-clone/pages/login.js
+++ spotify-clone/pages/login.js
@@ -1,9 +1,15 @@
 import { getProviders, signIn } from "next-auth/react"
 
-function Login() {
+function Login({ providers }) {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt=""/>
+
+      {providers && providers.length && Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={()=>signIn(provider.id, {callbackUrl: "/"})} className="bg-[#18D860] text-white p-5 rounded-full">Login with {provider.name}</button>
+        </div>
+      ))}
     </div>
   )
 }
