ad7fd35e15a68ad0782a47611e7b4a1ac1bba723
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-07 23:45:03 +0500

    Building search bar of header

--- airbnb-clone/components/Header.js
+++ airbnb-clone/components/Header.js
@@ -1,4 +1,6 @@
 import Image from "next/image"
+import { SearchIcon } from '@heroicons/react/solid'
+
 
 function Header() {
   return (
@@ -6,8 +8,9 @@ function Header() {
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image src="https://links.papareact.com/qd3" layout="fill" objectFit="contain" objectPosition="left"/>
       </div>
-      <div>
-        
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search"/>
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
       </div>
       <div>
         
