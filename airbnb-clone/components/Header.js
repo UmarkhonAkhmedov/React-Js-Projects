3ad28ae4f1d420cb2bcf9d99a6620591d759ab39
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-07 23:24:21 +0500

    Building left side of header

--- airbnb-clone/components/Header.js
+++ airbnb-clone/components/Header.js
@@ -1,8 +1,18 @@
+import Image from "next/image"
+
 function Header() {
   return (
-    <div>
-      
-    </div>
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image src="https://links.papareact.com/qd3" layout="fill" objectFit="contain" objectPosition="left"/>
+      </div>
+      <div>
+        
+      </div>
+      <div>
+        
+      </div>
+    </header>
   )
 }
 
