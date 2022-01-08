fb53c71ccb8f0d12aa9d91702ec07591ff05e4b7
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-08 13:49:09 +0500

    Building right side of header component

--- airbnb-clone/components/Header.js
+++ airbnb-clone/components/Header.js
@@ -1,5 +1,11 @@
 import Image from "next/image"
-import { SearchIcon } from '@heroicons/react/solid'
+import { 
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon
+} from '@heroicons/react/solid'
 
 
 function Header() {
@@ -12,8 +18,13 @@ function Header() {
         <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search"/>
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
       </div>
-      <div>
-        
+      <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer"/>
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6"/>
+          <UserCircleIcon className="h-6"/>
+        </div>
       </div>
     </header>
   )
