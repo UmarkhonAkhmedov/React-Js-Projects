f23019e836d08ce7a326a0b4b97eaaa4038d4028
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-08 14:30:05 +0500

    doing some server side rendering

--- airbnb-clone/pages/index.js
+++ airbnb-clone/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div>
       <Head>
@@ -13,6 +13,24 @@ export default function Home() {
 
       <Header/>
       <Banner/>
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          {exploreData.map(item => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
   )
 }
+
+// export async function getStaticProps(){
+//   const exploreData = await fetch("https//links.papareact.com/pyp").then((res)=>res.json());
+//   return {
+//     props: {
+//       exploreData
+//     }
+//   }
+// }
