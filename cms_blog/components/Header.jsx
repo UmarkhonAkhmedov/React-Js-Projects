7eeec09587fa1f165144a6de01487bdfdc25c452
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-14 04:18:04 +0500

    Adding and Styling Categories part of home page

--- cms_blog/components/Header.jsx
+++ cms_blog/components/Header.jsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { getCategories } from '../services'
 import Link from 'next/link'
 
-const categories = [
-  {name: 'React', slug: 'react'},
-  {name: 'Web Development', slug: 'web-dev'}
-]
 
 const Header = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories))
+  }, [])
+
   return (
     <div className='container mx-auto px-10 mb8'>
       <div className='border-b w-full inline-block border-blue-400 py-8'>
