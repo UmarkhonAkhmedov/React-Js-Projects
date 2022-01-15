365fb9923fba785dff0054017811b71a30f6861d
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-15 22:13:24 +0500

    Adding Carousel

--- cms_blog/services/index.js
+++ cms_blog/services/index.js
@@ -156,4 +156,28 @@ export const getComments = async (slug) => {
 
   return result.comments;
 };
+export const getFeaturedPosts = async () => {
+  const query = gql`
+    query GetCategoryPost() {
+      posts(where: {featuredPost: true}) {
+        author {
+          name
+          photo {
+            url
+          }
+        }
+        featuredImage {
+          url
+        }
+        title
+        slug
+        createdAt
+      }
+    }   
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
 
